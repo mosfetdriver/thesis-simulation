1e701b72c9baa097bc8ade2d267bb65ec4dc5131
Christian Santibáñez Soto <cm.santibanezsoto@gmail.com>
2024-05-28 13:05:51 -0400

    results storage ready

--- main/scenarios/bs.py
+++ main/scenarios/bs.py
@@ -1,8 +1,4 @@
 ### SCENARIO: GRID - CS - EXTERNAL LOAD ###
-#
-#
-#
-###
 
 # Libraries and other code
 import charging_station as cs
@@ -42,7 +38,7 @@ ev_id = n_cs * ['']
 
 # Datetime data to run the simulation
 start_datetime = datetime(year = 2025, month = 1, day = 1, hour = 0, minute = 0)
-end_datetime = datetime(year = 2025, month = 1, day = 31, hour = 23, minute = 59)
+end_datetime = datetime(year = 2025, month = 1, day = 2, hour = 23, minute = 59)
 time_interval = timedelta(minutes = 1)
 current_datetime = start_datetime
 current_date = start_datetime.date()
@@ -73,6 +69,8 @@ while(current_datetime <= end_datetime):
         if(str(current_datetime.time()) == ev_arrivals.loc[i, 't_dep']):
             for j in range(n_cs):
                 if(ev_id[j] == ev_arrivals.loc[i, 'id']):
+                    ev_ch_results = {'id': ev_id[j], 'e_dem': ev_dem[j], 'e_ch': ev_ch[j], 'satisfaction': ev_ch[j]/ev_dem[j], 'cp': f"cp{j}"}
+                    bs_evch_results = bs_evch_results._append(ev_ch_results, ignore_index = True)
                     ev_id[j] = ''
 
                     ev_dem[j] = 0.0
@@ -101,19 +99,27 @@ while(current_datetime <= end_datetime):
     # Function to allocate power to the EVs
     ev_pwr = FCI_ChSt.power_allocation(ev_dem, ev_ch, ev_tin, ev_tout, p_load, p_res, current_datetime.timestamp())
 
-    #print(ev_pwr)
     
-    # The amount of EVs present at the station is counted
+    
+    # The amount of EVs present at the station is counted and the charged energy is updated
     for i in range(n_cs):
+        ev_ch[i] += ev_pwr[i] * 0.01666666666
+
         if ev_id[i] != '':
             cs_occ_n += 1
-        
-    # The number is appended to a list (change to a pandas dataframe later)
-    cs_occ.append(cs_occ_n)
+
+    # Power data is stored
+    pwr_results = {'datetime': current_datetime, 'load': p_load[0], 'cs': sum(ev_pwr),
+                    'cp0': ev_pwr[0], 'cp1': ev_pwr[1], 'cp2': ev_pwr[2], 'cp3': ev_pwr[3], 'cp4': ev_pwr[4], 'cp5': ev_pwr[5],
+                      'cp6': ev_pwr[6], 'cp7': ev_pwr[7], 'cp8': ev_pwr[8], 'cp9': ev_pwr[9], 'cp10': ev_pwr[10], 'cp11': ev_pwr[11], 'n_ev': cs_occ_n}
+    
+    bs_pwr_results = bs_pwr_results._append(pwr_results, ignore_index = True)
 
     # The time is updated
     current_datetime += time_interval
     itr += 1
 
-plt.plot(cs_occ)
-plt.show()
+
+
+print(bs_pwr_results)
+print(bs_evch_results)
