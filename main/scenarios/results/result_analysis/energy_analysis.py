aa4fdb5139cd57f2be2b7e47959383820fc82342
Christian Santibáñez Soto <cm.santibanezsoto@gmail.com>
2024-06-26 18:37:16 -0400

    some changes

--- main/scenarios/results/result_analysis/energy_analysis.py
+++ main/scenarios/results/result_analysis/energy_analysis.py
@@ -10,9 +10,8 @@ scenario = scenarios[7]
 month = 12
 
 # Dataframe to store energy results
-energy_results = pd.DataFrame(columns = scenarios)
 summary = pd.DataFrame()
-summary['description'] = ["insat_evs", "sat_evs", "total_evs", "pct_to_bs", "cs_energy", "load_energy"]
+summary['description'] = ["insat_evs", "sat_evs", "total_evs", "pct_to_bs", "cs_energy", "load_energy", "res_energy", "grid_energy"]
 
 for col in range(len(scenarios)):
     summary[scenarios[col]] = ""
@@ -50,29 +49,7 @@ for i in range(8):
         sat_evs += sat_ch.shape[0]
         insat_evs += insat_ch.shape[0]
 
-        if(i == 0):
-            bs_energy.append(monthly_ch)
-        elif(i == 1):
-            wfa_energy.append(monthly_ch)
-        elif(i == 2):
-            std_energy.append(monthly_ch)
-        elif(i == 3):
-            s1_energy.append(monthly_ch)
-        elif(i == 4):
-            s2_energy.append(monthly_ch)
-        elif(i == 5):
-            s3_energy.append(monthly_ch)
-        elif(i == 6):
-            s4_energy.append(monthly_ch)
-        elif(i == 7):
-            s5_energy.append(monthly_ch)
-
-    pct = sat_evs / (sat_evs + (insat_evs - 316)) # -316
-    print("SCENARIO:", scenario)
-    print("EVs:", sat_evs + insat_evs,"-- SAT EVS:", sat_evs, "-- INSAT EVS:", insat_evs - 316, "-- PCT:", pct*100, "%")
-    print("CS ENERGY:", ev_ch_total, "[kWh]")
-    print("LOAD ENERGY:", load_total, "[kWh]")
-    print("--")
+    pct = sat_evs / (sat_evs + (insat_evs - 316)) # -316 is the base scenario number of insatisfied evs
 
     summary.loc[0, scenario] = insat_evs
     summary.loc[1, scenario] = sat_evs
@@ -80,24 +57,5 @@ for i in range(8):
     summary.loc[3, scenario] = pct
     summary.loc[4, scenario] = ev_ch_total
     summary.loc[5, scenario] = load_total
-
-energy_results['bs'] = bs_energy
-energy_results['wfa'] = wfa_energy
-energy_results['std'] = std_energy
-energy_results['s1'] = s1_energy
-energy_results['s2'] = s2_energy
-energy_results['s3'] = s3_energy
-energy_results['s4'] = s4_energy
-energy_results['s5'] = s5_energy
-
-
-bs_energy = energy_results['bs'].sum()
-wfa_energy = energy_results['wfa'].sum()
-std_energy = energy_results['std'].sum()
-s1_energy = energy_results['s1'].sum()
-s2_energy = energy_results['s2'].sum()
-s3_energy = energy_results['s3'].sum()
-s4_energy = energy_results['s4'].sum()
-s5_energy = energy_results['s5'].sum()
 
 print(summary)
